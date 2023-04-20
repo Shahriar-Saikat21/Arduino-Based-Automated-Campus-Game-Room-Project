9fd2ac3baf4866bcf25c3f1f9f99a94eb30cd5c0
Shifty Muhammad Ishmam <sishmam211040@bscse.uiu.ac.bd>
2023-04-20 19:00:22 +0600

    Door system final 1

--- Door_Code/Door/Door.ino
+++ Door_Code/Door/Door.ino
@@ -1,58 +1,75 @@
-#include <Servo.h>
+#include <SPI.h>
 #include <MFRC522.h>
+#include <Servo.h>
 
 #define SS_PIN 10
 #define RST_PIN 9
+#define servoPin 3
+#define LED_PIN 2
+
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
-#define SERVO_PIN 1
-#define LED_PIN 2
-
 Servo servo;
+
 int flag = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(LED_PIN, OUTPUT);  // set the LED pin as an output
-  servo.attach(SERVO_PIN);   // attach the servo to its pin
-  servo.write(0);            // set the initial position of the servo
-  SPI.begin();               // start the SPI communication
-  mfrc522.PCD_Init();        // initialize the RFID scanner
+  SPI.begin();
+  mfrc522.PCD_Init();
+  pinMode(LED_PIN, OUTPUT);
+  servo.attach(servoPin);
+  servo.write(0);
+  Serial.println("Scan Your Card Please.......");
+  Serial.println();  // initialize the RFID scanner
 }
 
 void loop() {
-
-  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
-
-    String uid = "";
-    for (byte i = 0; i < mfrc522.uid.size; i++) {
-      uid += String(mfrc522.uid.uidByte[i], HEX);
-    }
-
-    Serial.println("UID: " + uid);
-
-    if (uid == "27ad2cf") {
-
-      Serial.println("Access granted");
-
-      if (flag == 0) {
-        flag = 1;
-        servo.write(90);
-        digitalWrite(LED_PIN, HIGH);
-      } else {
-        flag = 0;
-        servo.write(0);
-        digitalWrite(LED_PIN, LOW);
-        delay(1000);
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+  Serial.print("UID tag :");
+  String content = "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "02 7A D2 CF") {
+    Serial.println("Authorized access....Door is opening.........");
+    
+    if (flag == 0) {
+      flag = 1;
+      for (int i = 0; i <= 90; i++) {
+        servo.write(i);
+        delay(15);
       }
-
-    } else {
-      Serial.println("Access denied");
-      delay(2000);
+      digitalWrite(LED_PIN, HIGH);
+      delay(100);
+    }else{
+      flag = 0;
+      for (int i = 90; i >= 0; i--) {
+        servo.write(i);
+        delay(15);
+      }
+      digitalWrite(LED_PIN, LOW);
+      delay(100);
     }
+  } else {
+    Serial.println("Access denied");
+    delay(500);
   }
 
   mfrc522.PICC_HaltA();
   mfrc522.PCD_StopCrypto1();
+
 }
