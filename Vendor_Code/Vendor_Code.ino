38e35195bdc06c0beca92cff069e47baa51258df
saikat <saikatbass@gmail.com>
2023-04-26 16:54:55 +0600

    Queue System Done V1.0

--- Vendor_Code/Vendor_Code.ino
+++ Vendor_Code/Vendor_Code.ino
@@ -1,6 +1,7 @@
 #include <SPI.h>
 #include <MFRC522.h>
 #include<Servo.h>
+#include <QueueArray.h>
  
 #define SS_PIN 10
 #define RST_PIN 9
@@ -13,6 +14,13 @@
 Servo s1;
 Servo s2;
 MFRC522 mfrc522(SS_PIN, RST_PIN);   
+
+unsigned long prev = 0;
+unsigned long cur = 0;
+
+QueueArray<int>q;
+int number = 1;
+int time = 0;
  
 void setup() 
 {
@@ -22,6 +30,7 @@ void setup()
   pinMode(BUTTON_PIN1, INPUT_PULLUP);
   pinMode(BUTTON_PIN2, INPUT_PULLUP);
   pinMode(BUTTON_PIN3, INPUT_PULLUP);
+  pinMode(7, OUTPUT);
   s1.attach(servoPin1);
   s2.attach(servoPin2);
   s1.write(0);
@@ -32,6 +41,23 @@ void setup()
 
 void loop() 
 {
+
+  cur = millis();
+
+  if(cur-prev>=1000){
+    if(!q.isEmpty ()){
+      time++;
+      digitalWrite(7,HIGH);
+    }
+    if(time==5){
+      int a = q.pop();
+      Serial.println(a);
+      time = 0;
+      digitalWrite(7,LOW);
+    }
+    prev=cur;
+  }
+
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
     return;
@@ -55,7 +81,7 @@ void loop()
   content.toUpperCase();
   if (content.substring(1) == "63 00 47 A8") 
   {
-    Serial.println("Authorized access....Select Your Box....");
+    Serial.println("Authorized access....Select Your Option....");
 
     int r = 0;
 
@@ -101,18 +127,21 @@ void loop()
           Serial.println("Scan Your Card Please......."); 
         }             
       }else if(buttonState3 == LOW ) {
-        Serial.println("Cancel....");
+        Serial.println("Entered your id in Queue....");
+        Serial.println(number);
+        q.enqueue (number);
+        number++;
         r = 1;  
         Serial.println("Scan Your Card Please.......");             
       }else {
         //Serial.println("Button is not pressed");
       }
-      delay(200);    
+      //delay(200);    
       }   
-    delay(500);
+   // delay(500);
   }else{
     Serial.println("Access denied");
-    delay(500);
+    //delay(500);
   } 
   
 }
